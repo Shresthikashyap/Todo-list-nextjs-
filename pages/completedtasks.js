45cd3f794cb5ad2ef00c53e15e82d478ae783c18
Shresthikashyap <122147931+Shresthikashyap@users.noreply.github.com>
2024-03-12 19:45:21 +0530

    dynamic route

---
+++ pages/completedtasks.js
@@ -0,0 +1,12 @@
+// pages/completedtask.js
+
+import Layout from '@/app/components/Layout';
+import CompletedTask from '../app/components/CompletedTask';
+
+export default function CompletedTaskPage() {
+  return (
+  <Layout>
+    <CompletedTask />
+  </Layout>
+   )
+}
